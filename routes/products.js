07b365d46bc5de72e9e8e0140a95fd24c94fee0f
Luis Quezada <luis.quezada881213@gmail.com>
2023-03-06 22:44:55 -0600

    add products to db

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,9 @@
-var route = require('express').Router();
+const route = require('express').Router();
+const db = require('better-sqlite3')('db.sqlite', { verbose: console.log });
 
 route.get("/", function(req, res){
-    res.send({})
+    const result = db.prepare('SELECT * FROM products').all();
+    res.send(result)
 })
 
 module.exports = route
